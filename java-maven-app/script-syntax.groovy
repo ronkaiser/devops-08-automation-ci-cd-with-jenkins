272e46afcf94841b270d4e36822e554b6c51eb7e
ron_kaiser <ron.kaiser@shopic.co>
2026-05-27 18:31:52 +0300

    Refactor Jenkinsfile to utilize external Groovy script for build, test, and deploy stages
    - Introduced a new initialization stage in the Jenkinsfile to load functions from script-syntax.groovy.
    - Updated build, test, and deploy stages to call respective functions from the loaded script, enhancing modularity and readability.

--- java-maven-app/script-syntax.groovy
+++ java-maven-app/script-syntax.groovy
@@ -1,5 +1,13 @@
-def function() {
+def buildApp() {
     echo 'building the application...'
 }
 
+def testApp() {
+    echo 'testing the application...'
+}
+
+def deployApp() {
+    echo 'deploying the application...'
+    echo "deploying version ${params.VERSION}"
+}
 return this
